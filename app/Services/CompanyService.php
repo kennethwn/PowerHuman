325b20d02816b575e1b0f172373a0711d678a28b
Kenneth <kenthnov117@gmail.com>
2023-08-23 10:28:50 +0700

    fix(CompanyService.php): remove all return value data type

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -12,15 +12,27 @@ class CompanyService {
         $this->repository = $repository;
     }
 
-    public function findCompanyById($id): Company {
+    public function createCompany($name, $logo): Company {
+        return $this->repository->createCompany($name, $logo);
+    }
+
+    public function updateCompany($companyById, $name, $logo) {
+        return $this->repository->updateCompany($companyById, $name, $logo);
+    }
+
+    public function findCompanyById($id) {
         return $this->repository->findCompanyById($id);
     }
 
-    public function findCompanyByName($name): Company {
+    public function findCompanyByName($name) {
         return $this->repository->findCompanyByName($name);
     }
 
-    public function getRelation() {
+    public function getRelationWithLoggedUser() {
+        return $this->repository->getRelationWithLoggedUser();
+    }
+
+    public function getRelationWithUser() {
         return $this->repository->getRelationWithUser();
     }
 }
