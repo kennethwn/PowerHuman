b54235d81ebe292522347c410c03dc2ece9dae44
Kenneth <kenthnov117@gmail.com>
2023-08-23 10:08:25 +0700

    fix(Company.php): remove parameter for pivot table in method users

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -18,7 +18,7 @@ class Company extends Model
     ];
 
     public function users(): BelongsToMany {
-        return $this->belongsToMany(User::class, 'company_user', 'user_id', 'company_id');
+        return $this->belongsToMany(User::class);
     }
 
     public function teams(): HasMany {
